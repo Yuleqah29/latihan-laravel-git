9777409ead8679b623739c4b0896df80c39cdaf5
Yuleqah29 <yuleqahrahmadani@gmail.com>
2023-01-02 10:56:14 +0700

    update pertama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,6 +100,9 @@
                         </div>
                     </div>
                 </div>
+               
+                <!--contoh perubahan -->
+                <!--      Laravel's robust library of first-party tools and libraries, such as  -->
 
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
